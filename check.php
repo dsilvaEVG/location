70b67d2356e1e957c7bb9567704ae99f002747f4
dsilvaEVG <dsilvavega.guadalupe@alumnado.fundacionloyola.net>
2024-10-07 10:23:24 +0200

    Update check.php

--- check.php
+++ check.php
@@ -1,14 +1,37 @@
 <!-- David Silva Vega -->
+
+<!-- Hacerlo en booleano -->
+
 <?php
-$num = $_GET["num"]; // Pasamos el dato del formulario y lo guardamos en la variable num
-$cuadrado = $num * $num; // Cálculo del cuadrado
+    $numero=$_GET["num"];
 
-if ($num % 2 == 0){ // Calculamos si el resto de la división entre 2 da 0. Si es true, el número es par
-    header("Location:par.php?num=$num&cuadrado=$cuadrado");
-}
+    // Función que calcula el cuadrado de un número pasado por parámetro
+    
+    
+    // Función booleana que devuelve true si el número pasado por parámetro par. Si no, devuelve falso
+    function par(&$numero){
 
-else { // Si es false, el número es impar
-    header("Location:impar.php?num=$num&cuadrado=$cuadrado");
-}
+        $numero=$numero*$numero; //Sobre el mismo número guardamos su cuadrado
+
+        if ($numero % 2 == 0){ 
+            return true; //Revisamos si el número al dividirlo entre 2 da resto 0. Si es cierto lo guardamos como true
+        }
+        else {
+            return false; //Si el resto no es 0, lo guardamos como false
+        }
+    }
+
+    
+
+    // Si la función par es true, dirige a la página de par
+    if (par($numero)){
+        header("Location:par.php?num=$numero");
+    }
+        
+    
+    // Si la función par es false, dirige a la página de impar
+    else {
+        header("Location:impar.php?num=$numero");
+    }
 
 ?>
